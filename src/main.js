61ad57cdf5009ba57943490fac2fad383df058b2
LAPTOP-43LHSDBV\73005 <730051080@qq.com>
2024-05-20 16:14:47 +0800

    feat: 移动端自适应配置

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import './assets/styles/main.less'
 import App from './App.vue'
 import router from './router'
 
+import 'lib-flexible/flexible'
+
 const app = createApp(App)
 
 app.use(router)
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,3 +3,11 @@
 </template>
 
 <script setup></script>
+
+<style lang="less" scoped>
+div {
+  border: 1px solid red;
+  width: 375px;
+  box-sizing: border-box;
+}
+</style>
